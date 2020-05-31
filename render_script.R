78d5d85d1f9e9d35a065806c10366f951351b620
dusty-turner <dusty.s.turner@gmail.com>
2020-05-30 20:34:04 -0400

    added functional programming chapter but book wont build

--- render_script.R
+++ render_script.R
@@ -3,3 +3,5 @@ bookdown::render_book("/Intermediate_R.Rmd", "bookdown::gitbook")
 # serve_book(dir = ".", output_dir = "_book", preview = TRUE, in_session = TRUE, quiet = FALSE)
 rmarkdown::render_site(encoding = 'UTF-8') #HTML and PDF
 
+
+bookdown::preview_chapter(input = "4-Text-Analysis.Rmd")
