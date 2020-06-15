9edaa4234f3fd35deba880b1c7fed1a13cac50a0
dusty-turner <dusty.s.turner.mil@mail.mil>
2020-06-15 09:36:06 -0400

    rendered book

--- render_script.R
+++ render_script.R
@@ -1,5 +1,5 @@
 library(bookdown)
-bookdown::render_book("Intermediate_R.Rmd", "bookdown::gitbook")
+bookdown::render_book("Intermediate_R.Rmd", "bookdown::gitbook",output_dir = "MORSIntermediateR_2020",clean_envir = TRUE)
 # serve_book(dir = ".", output_dir = "_book", preview = TRUE, in_session = TRUE, quiet = FALSE)
 # rmarkdown::render_site(encoding = 'UTF-8') #HTML and PDF
 
